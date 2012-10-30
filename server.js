ce1fac9170693b1037d92cf68ffdb9b4be7fe6d0
Kenneth Lee <kennethkl@gmail.com>
2012-10-30 14:41:08 -0400

    basic chat server

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-var http = require("http"),
-    url = require("url"),
-    path = require("path"),
-    fs = require("fs"),
-    port = process.argv[2] || 8080;
+var url = require("url");
+var path = require("path");
+var fs = require("fs");
+var port = process.argv[2] || 8080;
 
-http.createServer(function(request, response) {
-
+var httpHandler = function(request, response) {
   var uri = url.parse(request.url).pathname,
       filename = path.join(process.cwd(), 'server', uri);
 
@@ -32,6 +30,25 @@ http.createServer(function(request, response) {
       response.end();
     });
   });
-}).listen(parseInt(port, 10));
+};
 
+var app = require("http").createServer(httpHandler);
+var io = require('socket.io').listen(app);
+var sockets = {};
+
+io.sockets.on('connection', function (socket) {
+    socket.emit('chat', {name: '<b>Server</b>', message: 'Connected!'});
+    sockets[socket.id] = socket;
+    socket.on('chat', function (data) {
+        Object.keys(sockets).forEach(function(id) {
+            sockets[id].emit('chat', data);
+        });
+    });
+});
+
+io.sockets.on('disconnect', function (socket) {
+    delete sockets[socket.id];
+});
+
+app.listen(parseInt(port, 10));
 console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
